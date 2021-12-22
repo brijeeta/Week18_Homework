14971d646169446d38cdeeb210d9bfd0dcaab786
Holy Paul <brijeeta@gmail.com>
2021-12-22 16:58:53 -0500

    added total duration to the schema,added html.js,range route

--- models/Workout.js
+++ models/Workout.js
@@ -2,11 +2,43 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({})
+const workoutSchema = new Schema({
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: Number,
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ],
+    totalDuration: {
+        type: Number,
+    }
 
 
-
-
+})
 
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/api.js
+++ routes/api.js
@@ -23,17 +23,30 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 
+router.put("/api/workouts/:id", (req, res) => {
+    // update workout 
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration }, $push: { exercises: req.body }
+        },
+        { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
 
 
-
-
-
-
-
-
-
-
-
-
+router.get("/api/workouts/range", (req, res) => {
+    // find all workouts
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
 
 module.exports = router;
